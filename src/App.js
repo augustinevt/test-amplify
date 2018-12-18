306539b5ce25412891f0c0989b3c10680758ab80
august von trapp <augustvontrapp@gmail.com>
2018-12-17 23:46:03 -0800

    successful trail

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Amplify, {Storage} from 'aws-amplify';
+import {Button} from 'antd';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
+
 import './App.css';
 
+Amplify.configure(aws_exports);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button onClick={this.doSomething}> Do Something </Button>
+        { this.state.data.map(item => <div>{item}</div>)}
       </div>
     );
   }
+
+  doSomething = async () => {
+    try {
+      const results = await Storage.get('testJson.json');
+      const fetchResults = await fetch(results);
+      const result = await fetchResults.json();
+
+      this.setState({data: result.data})
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
 }
 
-export default App;
+export default withAuthenticator(App);
